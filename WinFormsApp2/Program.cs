56d72b53caa40742b3503ae91534ef86b2b6aae2
Egor Usikov <huawei2610176@gmail.com>
2026-02-28 17:55:17 +0300

    second

--- WinFormsApp2/Program.cs
+++ WinFormsApp2/Program.cs
@@ -2,9 +2,7 @@ namespace WinFormsApp2;
 
 static class Program
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
+    
     [STAThread]
     static void Main()
     {
